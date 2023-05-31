08b4b50bfb3fc1e344d62e90e0718bab1845b2dc
Rhythm01~ <robiulhaque1919@gmail.com>
2023-06-01 03:16:35 +0600

    updated login and signup page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -70,17 +70,9 @@ const Login = () => {
       <Helmet>
         <title>Bistro Boss | Login</title>
       </Helmet>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col md:flex-row-reverse">
-          <div className="text-center md:w-1/2 lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
+      <div className="hero min-h-screen pt-10 bg-base-200">
           <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+            <h1 className="text-5xl font-bold mx-auto my-5">Login now!</h1>
             <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -130,14 +122,13 @@ const Login = () => {
                 />
               </div>
             </form>
-            <p>
+            <p className="text-center">
               <small>
-                New Here? <Link to="/signup">Create an account</Link>{" "}
+                New Here? <Link to="/signup" className="text-orange-500">Create an account</Link>{" "}
               </small>
             </p>
             <SocialLogin></SocialLogin>
           </div>
-        </div>
       </div>
     </>
   );
